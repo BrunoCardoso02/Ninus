b8c66027fabf7e0e07c5b597ecc8c097b1f919af
Bruno <bruno.cardoso02@outlook.com>
2023-09-08 15:32:13 -0300

    Tela de cadastro de conta atualizada

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import TelaLogin from '../Ninus/components/TelaLogin/index.js'
+import TelaLogin from '../Ninus/components/TelaLogin/index.js';
+import CadastroUsuario from '../Ninus/components/CadastroUsuario/index.js';
+import TelaAulas from '../Ninus/components/TelaAulas/index.js'
 
 export default function App() {
   return (
-    <TelaLogin/>
+    <CadastroUsuario/>
   );
 }
 
